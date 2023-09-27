8d47ab6051c0fb2141fec42b03f23b5ad242e10a
ChatGTHB <cihat.kose@hotmail.com>
2023-09-27 08:31:24 +0200

    Updated Hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -8,10 +8,12 @@ public class Hooks {
 
     @Before
     public void before() {
+        System.out.println("The scenario has begun.");
     }
 
     @After
     public void after() {
+        System.out.println("The scenario is finished.");
         GWD.quitDriver();
     }
 }
