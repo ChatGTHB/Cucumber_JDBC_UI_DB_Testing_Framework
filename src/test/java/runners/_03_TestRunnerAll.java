be8d5d58c9e077784dc07e9fe09c4cbc9f2fb1f9
ChatGTHB <cihat.kose@hotmail.com>
2023-09-30 17:54:07 +0200

    Added _03_TestRunnerAll.java

---
+++ src/test/java/runners/_03_TestRunnerAll.java
@@ -0,0 +1,11 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"}
+)
+public class _03_TestRunnerAll extends AbstractTestNGCucumberTests {
+}
