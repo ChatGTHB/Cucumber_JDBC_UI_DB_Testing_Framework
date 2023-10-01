0b933a34230f72e2620c4d0e958584fe27250787
ChatGTHB <cihat.kose@hotmail.com>
2023-10-01 18:39:33 +0200

    Added _07_TestRunnerExtentReport.java

---
+++ src/test/java/runners/_07_TestRunnerExtentReport.java
@@ -0,0 +1,29 @@
+package runners;
+
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+
+@CucumberOptions(
+        tags = "@Regression",
+        features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("Windows Username", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("Username", "Cihat K.");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+    }
+}
