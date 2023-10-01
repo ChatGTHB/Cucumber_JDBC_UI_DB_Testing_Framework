a07cc0429dfe99d8fc260aaedc6b83439be56f48
ChatGTHB <cihat.kose@hotmail.com>
2023-10-01 18:37:14 +0200

    Updated Hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,6 +2,10 @@ package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.ExcelUtility;
 import utilities.GWD;
 
 public class Hooks {
@@ -12,7 +16,16 @@ public class Hooks {
     }
 
     @After
-    public void after() {
+    public void after(Scenario scenario) {
+//        ExcelUtility.writeToExcel("src/test/java/ApachePOI/resource/ScenarioResults3.xlsx",
+//                scenario);
+
+        // Keep the extent report's plugin open while it is active
+        if (scenario.isFailed()){
+            TakesScreenshot screenshot=((TakesScreenshot) GWD.getDriver());
+            byte[] stateInMemory=screenshot.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(stateInMemory, "image/png", "screenshot name");
+        }
         System.out.println("The scenario finished.");
         GWD.quitDriver();
     }
