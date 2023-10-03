05199d36bf7b7b8a1993e79297f008a30d73a6bb
ChatGTHB <cihat.kose@hotmail.com>
2023-10-03 22:04:57 +0200

    Updated _06_TestRunnerSmokeWithPlugin.java

--- src/test/java/runners/_06_TestRunnerSmokeWithPlugin.java
+++ src/test/java/runners/_06_TestRunnerSmokeWithPlugin.java
@@ -3,12 +3,13 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        tags = "@SmokeTest",
-        features = {"src/test/java/featureFiles"},
-        glue = {"stepDefinitions"},
-        plugin = {"html:target/site/cucumber-pretty.html"},
-        dryRun = false
-)
+@CucumberOptions
+        (
+                tags = "@SmokeTest",
+                features = {"src/test/java/featureFiles"},
+                glue = {"stepDefinitions"},
+                plugin = {"html:target/site/cucumber-pretty.html"},
+                dryRun = false
+        )
 public class _06_TestRunnerSmokeWithPlugin extends AbstractTestNGCucumberTests {
 }
