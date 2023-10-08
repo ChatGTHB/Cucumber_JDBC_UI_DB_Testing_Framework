869e14d89d993810b30d298f02a27977643bd8cb
ChatGTHB <cihat.kose@hotmail.com>
2023-10-08 18:33:19 +0200

    Added _05_WriteInTheExcel.java

---
+++ src/test/java/apachePOI/_05_WriteInTheExcel.java
@@ -0,0 +1,32 @@
+package apachePOI;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _05_WriteInTheExcel {
+    public static void main(String[] args) throws IOException {
+
+        String path = "src/test/java/apachePOI/resource/WriteInTheExcel.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        int lastRowIndex=sheet.getPhysicalNumberOfRows();
+        Row newRow=sheet.createRow(lastRowIndex);
+        Cell newCell=newRow.createCell(0);
+        newCell.setCellValue("Hello World");
+
+        fileInputStream.close();
+
+        FileOutputStream fileOutputStream=new FileOutputStream(path);
+        workbook.write(fileOutputStream);
+
+        workbook.close();
+        fileOutputStream.close();
+
+        System.out.println("The process is over.");
+    }
+}
