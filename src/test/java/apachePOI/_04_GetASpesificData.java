9105295e5d8d7945949fd4adbd808e5d4c0fdb2e
ChatGTHB <cihat.kose@hotmail.com>
2023-10-08 18:09:45 +0200

    Updated _04_GetASpesificData.java

--- src/test/java/apachePOI/_04_GetASpesificData.java
+++ src/test/java/apachePOI/_04_GetASpesificData.java
@@ -50,5 +50,22 @@ public class _04_GetASpesificData {
             }
         }
         return willReturn;
+
+        /** The Second Way --- With StringBuilder*/
+//        StringBuilder willReturn = new StringBuilder();
+//
+//        String path = "src/test/java/apachePOI/resource/LoginData.xlsx";
+//        FileInputStream fileInputStream = new FileInputStream(path);
+//        Workbook workbook = WorkbookFactory.create(fileInputStream);
+//        Sheet sheet = workbook.getSheetAt(0);
+//
+//        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+//            if (sheet.getRow(i).getCell(0).toString().toLowerCase().contains(searchWord.toLowerCase())) {
+//                for (int j = 1; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+//                    willReturn.append(sheet.getRow(i).getCell(j));
+//                }
+//            }
+//        }
+//        return willReturn.toString();
     }
 }
