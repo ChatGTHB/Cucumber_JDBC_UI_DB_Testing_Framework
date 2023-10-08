cc0c9080885206900d788aab172d6644471498db
ChatGTHB <cihat.kose@hotmail.com>
2023-10-08 18:06:39 +0200

    Updated _03_ApachePOIGetAllData.java

--- src/test/java/apachePOI/_03_ApachePOIGetAllData.java
+++ src/test/java/apachePOI/_03_ApachePOIGetAllData.java
@@ -12,15 +12,25 @@ public class _03_ApachePOIGetAllData {
         FileInputStream fileInputStream = new FileInputStream(path);
         Workbook workbook = WorkbookFactory.create(fileInputStream);
 
-        Sheet sheet = workbook.getSheetAt(0);
+        Sheet sheet = workbook.getSheetAt(0); // Sheet sheet=workbook.getSheet("Sheet1");
         int rowCount = sheet.getPhysicalNumberOfRows();
 
+        // The First Way
         for (int i = 0; i < rowCount; i++) {
             Row row = sheet.getRow(i);
             int cellCount = row.getPhysicalNumberOfCells();
             for (int j = 0; j < cellCount; j++) {
                 Cell cell = row.getCell(j);
                 System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("*****************************************************");
+        // The Second Way
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+            for (int j = 0; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+                System.out.print(sheet.getRow(i).getCell(j) + "\t");
             }
             System.out.println();
         }
