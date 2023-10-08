7ca82e71d68868850df9e79f8f05e6330e5c66b0
ChatGTHB <cihat.kose@hotmail.com>
2023-10-08 22:03:26 +0200

    Added _08_Question_02.java

---
+++ src/test/java/apachePOI/_08_Question_02.java
@@ -0,0 +1,47 @@
+package apachePOI;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _08_Question_02 {
+    public static void main(String[] args) throws IOException {
+        /**
+         *  Yeni bir excel dosyasına çarpım tablosunu
+         *  alt alta yazdırınız.
+         *
+         *  -------------------------------------------------------------------
+         *
+         *  Copy the multiplication table into a new excel file
+         *  print below.
+         */
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+
+        int newRowIndex = 0; // sheet.getPhysicalNumberOfRows();
+
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= 10; j++) {
+                Row row = sheet.createRow(newRowIndex++);
+
+                row.createCell(0).setCellValue(i);
+                row.createCell(1).setCellValue("x");
+                row.createCell(2).setCellValue(j);
+                row.createCell(3).setCellValue("=");
+                row.createCell(4).setCellValue(i * j);
+            }
+            Row nextRow = sheet.createRow(newRowIndex++);
+        }
+
+        String path = "src/test/java/apachePOI/resource/NewMultiplicationTable.xlsx";
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        workbook.write(fileOutputStream);
+        workbook.close();
+        fileOutputStream.close();
+
+        System.out.println("The process is over.");
+    }
+}
