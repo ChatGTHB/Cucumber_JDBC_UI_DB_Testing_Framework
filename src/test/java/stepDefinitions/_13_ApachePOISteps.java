fe52282202cadf33264e02b333ce2b7d1806d4fe
ChatGTHB <cihat.kose@hotmail.com>
2023-10-09 12:23:49 +0200

    Added _13_ApachePOISteps.java

---
+++ src/test/java/stepDefinitions/_13_ApachePOISteps.java
@@ -0,0 +1,45 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+import pages.DialogContent;
+import utilities.ExcelUtility;
+
+import java.util.ArrayList;
+
+public class _13_ApachePOISteps {
+
+    DialogContent dc = new DialogContent();
+
+    @When("User create citizenship with ApachePOI")
+    public void userCreateCitizenshipWithApachePOI() {
+
+        ArrayList<ArrayList<String>> table =
+                ExcelUtility.getData
+                        ("src/test/java/ApachePOI/resource/ApacheExcel.xlsx",
+                                "testCitizen", 2);
+
+        for (int i = 0; i < table.size(); i++) {
+            ArrayList<String> row = table.get(i);
+            dc.myClick(dc.addButton);
+            dc.mySendKeys(dc.nameInput, row.get(0));
+            dc.mySendKeys(dc.shortName, row.get(1));
+            dc.myClick(dc.saveButton);
+            dc.verifyContainsText(dc.successMessage, "success");
+        }
+    }
+
+    @Then("User delete citizenship with ApachePOI")
+    public void userDeleteCitizenshipWithApachePOI() {
+
+        ArrayList<ArrayList<String>> table =
+                ExcelUtility.getData
+                        ("src/test/java/ApachePOI/resource/ApacheExcel.xlsx",
+                                "testCitizen", 2);
+
+        for (int i = 0; i < table.size(); i++) {
+            ArrayList<String> row = table.get(i);
+            dc.deleteItem(row.get(0));
+            dc.verifyContainsText(dc.successMessage, "success");
+        }
+    }
+}
