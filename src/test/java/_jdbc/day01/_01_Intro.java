b5a150ab36485db90010875514382a1bba386ac3
ChatGTHB <cihat.kose@hotmail.com>
2023-10-23 21:06:17 +0200

    Added _01_Intro.java for JDBC operations

---
+++ src/test/java/_jdbc/day01/_01_Intro.java
@@ -0,0 +1,44 @@
+package _jdbc.day01;
+
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class _01_Intro {
+    @Test
+    public void test1() throws SQLException {
+
+        // What is done to make a query work
+        String hostUrl = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com/sakila";
+        String username = "root";
+        String password = "'\"-LhCB'.%k[4S]z";
+
+        // 1)- We entered the connection information --> Connection information was set.
+        Connection connection= DriverManager.getConnection(hostUrl,username,password);
+
+        // 2)- We have chose the database --> This information was added to hostUrl (/sakila); no other additional action was taken
+
+        // 3)- We opened the query screen
+        Statement statement= connection.createStatement();
+
+        // 4)- We wrote the query to query screen then I was run
+        ResultSet resultTable=statement.executeQuery("select * from customer");
+
+        // 5)- The results appeared below
+        resultTable.next(); // At this stage we have completed the first line
+
+        String name=resultTable.getString("first_name");   // I got the information he wanted in this line
+        String surname=resultTable.getString("last_name"); // I got the information he wanted in this line
+        System.out.println("name = " + name);
+        System.out.println("surname = " + surname);
+
+        resultTable.next(); // At this stage we have completed the second line
+
+        name=resultTable.getString("first_name");   // I got the information he wanted in this line
+        surname=resultTable.getString("last_name"); // I got the information he wanted in this line
+        System.out.println("name = " + name);
+        System.out.println("surname = " + surname);
+
+        connection.close();
+    }
+}
