174963d9d5d96030a38407ffbe2ec0d07b1ffdf9
ChatGTHB <cihat.kose@hotmail.com>
2023-10-23 22:28:50 +0200

    Added _02_Intro.java for JDBC operations

---
+++ src/test/java/_jdbc/day01/_02_Intro.java
@@ -0,0 +1,25 @@
+package _jdbc.day01;
+
+import _jdbc.JDBCParent;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class _02_Intro extends JDBCParent {
+
+    @Test
+    public void test1() throws SQLException {
+
+        ResultSet resultTable = statement.executeQuery("select * from customer");
+
+        resultTable.next(); // At this stage we have completed the first line
+        System.out.println(resultTable.getString("first_name"));//
+        System.out.println(resultTable.getString("last_name"));
+
+        System.out.println("*************************************************");
+
+        resultTable.next(); // At this stage we have completed the second line
+        System.out.println(resultTable.getString("first_name"));
+        System.out.println(resultTable.getString("last_name"));
+    }
+}
