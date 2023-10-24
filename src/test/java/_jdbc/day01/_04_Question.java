c04050cca710cf2da65897f4c180491d11994dc6
ChatGTHB <cihat.kose@hotmail.com>
2023-10-24 13:59:15 +0200

    Added _04_Question.java for JDBC operations

---
+++ src/test/java/_jdbc/day01/_04_Question.java
@@ -0,0 +1,26 @@
+package _jdbc.day01;
+
+import _jdbc.JDBCParent;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _04_Question extends JDBCParent {
+    /**
+     * Sakila database içerisinde yer alan city tablosundan tüm satırlardaki şehir isimlerini next ile yazdırınız.
+     * ---------------------------------------------------------------------------------------------------------------
+     * Print the city names in all rows from the city table in the Sakila database with next.
+     */
+
+    @Test
+    public void cityTest() throws SQLException {
+
+        ResultSet resultTable = statement.executeQuery("select * from city");
+
+        while (resultTable.next()) {
+            System.out.println("City Name = " + resultTable.getString("city"));
+        }
+    }
+}
+
