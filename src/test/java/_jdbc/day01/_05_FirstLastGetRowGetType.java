761ae9fd70e824b96b505a630899da4938a820db
ChatGTHB <cihat.kose@hotmail.com>
2023-10-24 14:14:27 +0200

    Added _05_FirstLastGetRowGetType.java for JDBC operations

---
+++ src/test/java/_jdbc/day01/_05_FirstLastGetRowGetType.java
@@ -0,0 +1,26 @@
+package _jdbc.day01;
+
+import _jdbc.JDBCParent;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _05_FirstLastGetRowGetType extends JDBCParent {
+
+    @Test
+    public void test01() throws SQLException {
+
+        ResultSet resultTable = statement.executeQuery("select city from city");
+
+        resultTable.last();  // Goes to the last line
+        System.out.println("City name at the last line = " + resultTable.getString(1));
+
+        resultTable.first(); // Goes to the first line
+        System.out.println("City name at the first line = " + resultTable.getString(1));
+
+        resultTable.last();  // Goes to the last line
+        int whichRowAmIOn = resultTable.getRow(); // Get the row number I'm on --> The last line
+        System.out.println("The Line I'm On = " + whichRowAmIOn);
+    }
+}
