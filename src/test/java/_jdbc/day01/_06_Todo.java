4b78bfd60a1db8873609db6a92c07eceefc9f081
ChatGTHB <cihat.kose@hotmail.com>
2023-10-24 14:40:27 +0200

    Added _06_Todo.java for JDBC operations

---
+++ src/test/java/_jdbc/day01/_06_Todo.java
@@ -0,0 +1,49 @@
+package _jdbc.day01;
+
+import _jdbc.JDBCParent;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _06_Todo extends JDBCParent {
+    /**
+     TODO: Sakila database içerisinde yer alan city tablosundan tüm satırlardaki şehir isimlerini absolute ile yazdırınız
+     TODO: Sakila database içerisinde yer alan city tablosundan tüm satırlardaki şehir isimlerini relative ile yazdırınız
+     ----------------------------------------------------------------------------------
+     TODO: Print the city names in all rows from the city table in the Sakila database with absolute.
+     TODO: Print the city names in all rows from the city table in the Sakila database with relative.
+     */
+
+    @Test
+    public void absoluteTest() throws SQLException {
+
+        ResultSet resultTable = statement.executeQuery("select * from city");
+
+        resultTable.last();
+        int lastRowNumber = resultTable.getRow();
+
+        for (int i = 1; i <= lastRowNumber; i++) {
+            resultTable.absolute(i);
+            System.out.println("City name with absolute = " + resultTable.getString("city"));
+            System.out.println("i = " + i);
+        }
+    }
+
+    @Test
+    public void relativeTest() throws SQLException {
+
+        ResultSet resultTable = statement.executeQuery("select * from city");
+
+        resultTable.last();
+        int lastRowNumber = resultTable.getRow();
+
+        resultTable.first();
+        int firstRowNumber= resultTable.getRow();
+
+        for (int i = 0; i < lastRowNumber; i++) {
+            System.out.println("City name with relative = " + resultTable.getString("city"));
+            resultTable.relative(1);
+        }
+    }
+}
