935addb7ab4f220254b1f4997a2457361053cd58
ChatGTHB <cihat.kose@hotmail.com>
2023-11-27 11:19:22 +0100

    Added plugin to _04_TestRunnerSmoke.java class to get a report when run through Jenkins

--- src/test/java/runners/_04_TestRunnerSmoke.java
+++ src/test/java/runners/_04_TestRunnerSmoke.java
@@ -1,12 +1,12 @@
 package runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.*;
 
 @CucumberOptions(
         tags = "@SmokeTest",
         features = {"src/test/java/featureFiles"},
-        glue={"stepDefinitions"}
+        glue={"stepDefinitions"},
+        plugin= {"json:target/cucumber/cucumber.json"} // JSON report for Jenkins
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
