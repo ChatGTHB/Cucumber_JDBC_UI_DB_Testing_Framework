02a14bb70aa034ab1ffc84168fdaefc08becbbc1
ChatGTHB <cihat.kose@hotmail.com>
2023-11-28 10:38:03 +0100

    Added plugin to _05_TestRunnerRegression.java class to get a report when run through Jenkins

--- src/test/java/runners/_05_TestRunnerRegression.java
+++ src/test/java/runners/_05_TestRunnerRegression.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest or @Regression",
         features = {"src/test/java/featureFiles"},
-        glue={"stepDefinitions"}
+        glue = {"stepDefinitions"},
+        plugin = {"json:target/cucumber/cucumber.json"} // JSON report for Jenkins
 )
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
