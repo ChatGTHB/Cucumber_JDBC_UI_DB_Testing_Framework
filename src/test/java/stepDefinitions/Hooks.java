01c9d2a52a7fe0eac8f7e9abc7e30c4a027d2328
ChatGTHB <cihat.kose@hotmail.com>
2024-03-27 13:32:36 +0100

    Edited Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -8,7 +8,7 @@ public class Hooks {
 
     @Before
     public void before() {
-        System.out.println("The scenario started.");
+        System.out.println("The scenario has started.");
     }
 
     @After
