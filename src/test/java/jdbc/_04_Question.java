c15d334ec0d34a5f293268a670cf45544f10f2c0
ChatGTHB <cihat.kose@hotmail.com>
2024-11-10 16:57:25 +0100

    Edit

--- src/test/java/jdbc/_04_Question.java
+++ src/test/java/jdbc/_04_Question.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 
 public class _04_Question extends JDBCParent {
     /**
-     * Sakila database içerisinde yer alan city tablosundan tüm satırlardaki şehir isimlerini next ile yazdırınız.
-     * ---------------------------------------------------------------------------------------------------------------
-     * Print the city names in all rows from the city table in the Sakila database with next.
+        Sakila database içerisinde yer alan city tablosundan tüm satırlardaki şehir isimlerini next ile yazdırınız.
+        ---------------------------------------------------------------------------------------------------------------
+        Print the city names in all rows from the city table in the Sakila database with next.
      */
 
     @Test
@@ -22,4 +22,5 @@ public class _04_Question extends JDBCParent {
         }
     }
 }
+
 
