08f60a93c92810ca2511f6bf026cdb49e28c8d38
ChatGTHB <cihat.kose@hotmail.com>
2024-11-21 18:56:06 +0100

    The project has been updated

--- src/test/java/runners/_01_TestRunner.java
+++ src/test/java/runners/_01_TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/featureFiles/_01_Login.feature"},
-        glue = {"stepDefinitions"}
+        glue = {"stepDefinitions"},
+        plugin= {"json:targetcucumbercucumber.json"} // JSON report for Jenkins
 )
 public class _01_TestRunner extends AbstractTestNGCucumberTests {
 }
